9bf484fdd2f48020d588e6a46bc732bc61f46430
tsuji <t.517a3@gmail.com>
2024-10-04 17:14:28 +0900

    add: ginフレームワーク用のファイルを追加、既存ファイルに追記

--- poke-quick-back/main.go
+++ poke-quick-back/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -13,4 +15,12 @@ func main() {
     fmt.Println("Hello, Poke Quick!")
     http.HandleFunc("/", handler)
     http.ListenAndServe(":8080", nil)
+
+    router := gin.Default()
+    router.GET("/", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{
+            "message": "Hello, World!",
+        })
+    })
+    router.Run(":8080")
 }
